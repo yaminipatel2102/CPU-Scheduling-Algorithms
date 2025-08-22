7b15ce06f6910e24b103880a911dfdfc8ba8ec3d
yaminipatel2102 <yaminipambhar@gmail.com>
2025-08-22 20:26:26 +0530

    Create cd.js

---
+++ cd.js
@@ -0,0 +1,6 @@
+function clearData() {
+    document.getElementById('gainchart').innerHTML = "";
+    document.getElementById('outputTable').innerHTML = "";
+    document.getElementById('analysis').innerHTML = "";
+    document.getElementById('clear').innerHTML="";
+}
